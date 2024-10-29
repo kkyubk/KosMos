5dc00b5cdd41b9a54e5c584e6e24a2f7fb0fd397
kkyubk <166334419+kkyubk@users.noreply.github.com>
2024-10-29 23:19:42 +0300

    Add files via upload

---
+++ Registration.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+
+namespace Desktop
+{
+   
+    public class InputValidator
+    {
+        public static bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Here we use regular expression to validate email
+                return Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            return !string.IsNullOrWhiteSpace(password) && password.Length >= 6;
+        }
+
+        public static bool ValidateName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name) && name.Length >= 3;
+        }
+    }
+}
+
